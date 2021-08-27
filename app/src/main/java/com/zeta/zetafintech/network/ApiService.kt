bc10d6583b17bce028a0b30bedbfdd66e2d056c4
Aditya V Jajodia <aditya.v.jajodia@gmail.com>
2021-08-27 10:45:44 +0530

    Sample POST request created for reference.

--- app/src/main/java/com/zeta/zetafintech/network/ApiService.kt
+++ app/src/main/java/com/zeta/zetafintech/network/ApiService.kt
@@ -1,5 +1,6 @@
 package com.zeta.zetafintech.network
 
+import com.zeta.zetafintech.models.account.AccountDetails
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Field
@@ -7,4 +8,9 @@ import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface ApiService {
+
+    // Sample POST request for reference
+//    @POST("user/login")
+//    fun doLogin(@Body loginRequest: AccountDetails): Call<AccountDetails>
+
 }
