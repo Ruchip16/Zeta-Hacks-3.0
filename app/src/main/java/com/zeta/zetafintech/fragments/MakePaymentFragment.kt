7785e85845139dfaca81993109f6ab93a561ae07
Aarushi Shanker <aarushishanker3108@gmail.com>
2021-08-27 12:53:23 +0530

    project done

--- app/src/main/java/com/zeta/zetafintech/fragments/MakePaymentFragment.kt
+++ app/src/main/java/com/zeta/zetafintech/fragments/MakePaymentFragment.kt
@@ -9,6 +9,9 @@ import androidx.fragment.app.Fragment
 import com.zeta.zetafintech.R
 import com.zeta.zetafintech.Utils
 import com.zeta.zetafintech.databinding.FragmentMakePaymentBinding
+import com.zeta.zetafintech.models.account.Amount
+import com.zeta.zetafintech.models.account.PaymentReq
+import com.zeta.zetafintech.network.WebService
 
 class MakePaymentFragment : Fragment() {
 
@@ -25,7 +28,15 @@ class MakePaymentFragment : Fragment() {
         val root: View = binding.root
 
         Utils.blackIconsStatusBar(activity, R.color.light_bg)
-
+        var amt = binding.amountpay.text.toString()
+        var acc = binding.accNo.text.toString()
+        binding.scanbutton.setOnClickListener {
+            makePay(amt, acc)
+        }
         return root
+    }
+
+    fun makePay(amt: String, acc: String){
+        WebService.apiService.doPayment(PaymentReq(Amount(amt.toInt(),"INR"),null, acc, "b4fc13fd-3e0f-43c6-81c0-47ced3a00893","", "","", null))
     }
 }
--- app/src/main/java/com/zeta/zetafintech/network/ApiService.kt
+++ app/src/main/java/com/zeta/zetafintech/network/ApiService.kt
@@ -1,16 +1,19 @@
 package com.zeta.zetafintech.network
 
 import com.zeta.zetafintech.models.account.AccountDetails
+import com.zeta.zetafintech.models.account.PaymentReq
+import com.zeta.zetafintech.models.account.PaymentRes
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface ApiService {
 
     // Sample POST request for reference
-//    @POST("user/login")
-//    fun doLogin(@Body loginRequest: AccountDetails): Call<AccountDetails>
+    @Headers(
+        "X-Zeta-AuthToken:eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwidGFnIjoicmZCOEpDZld4N0dxYkVib2FWWEFUQSIsImFsZyI6IkExMjhHQ01LVyIsIml2IjoiZlNyakYxZDFOaXFVZkFiZiJ9.8bZ8VWzecCgA-JIXC1HSkPfsf0qTxVwpl3IG5_ZXEv8.2dMBi1C9cfiPj4kI-leV0w.tI9Jd9aDBx_IUUCPOFr3_vZjlSWy5ldjW4fXOGXtw8IN06NSL9Lzm36DicFHNGe6lZ1nsEW2pWdlQEbK_3iNIG051ZS5KNgyCM3eocO7r07DrGfEF7or3DrV3QzHfHd1WwzkYOuK2vi2hbYSYYllFj48Dorbn0lnZVOi6EOY81eOqXv-uLZk77iEV_B8kXeijpvBcaGxOR68Rkr15xaNYKiEFzKIrSd-Ng0z-6K0SXgZAs3C5WUz0ENC1HKEvCYEMf1aXjH2VI20r4pjnG0FSiTrvyPz8m6hcuJSu9DuktZKRFcm"
+
+    )
+    @POST("transfers")
+    fun doPayment(@Body loginRequest: PaymentReq): Call<PaymentRes>
 
 }
